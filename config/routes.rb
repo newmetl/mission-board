832b6dc1a1ae6f1ae4e6db3691978df4d60a9dbd
Wojtek Gorecki <adalbert.gorecki@gmail.com>
2017-02-06 13:49:42 +0100

    Add exit button and current user name in navigation

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :create]
   resources :roles
   get '/missionboard' => 'missionboard#show', as: 'missionboard'
+  root 'users#index'
 end
