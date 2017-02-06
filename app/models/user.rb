44f826943da0a5c66de2102448aaf92596c02552
Wojtek Gorecki <adalbert.gorecki@gmail.com>
2017-02-06 14:33:55 +0100

    Add user photo upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+  mount_uploader :photo, FileUploader
   validates :name, presence: true, length: { minimum: 3 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206101304) do
+ActiveRecord::Schema.define(version: 20170206131555) do
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170206101304) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "photo"
   end
 
 end
