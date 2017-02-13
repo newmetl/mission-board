de80ea8b7dd536cc0ba665474b2fb56c68a9d223
Wojtek Gorecki <adalbert.gorecki@gmail.com>
2017-02-13 15:43:24 +0100

    Nice moods list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  MOOD_CLASSES = {
+    'green' => 'list-group-item-success',
+    'yellow' => 'list-group-item-warning',
+    'red' => 'list-group-item-danger'
+  }
+
+  def class_for_mood(mood)
+    MOOD_CLASSES[mood.name]
+  end
 end
