956dffd9d6f41af12150d51a9efe57fb96884cc0
Wojtek Gorecki <adalbert.gorecki@gmail.com>
2017-04-28 17:02:48 +0200

    Make user mood optional;

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :todos
   has_and_belongs_to_many :boards
-  belongs_to :mood
+  belongs_to :mood, optional: true
   mount_uploader :photo, ::FileUploader
   validates :name, presence: true, length: { minimum: 3 }
 end
